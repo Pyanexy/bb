6c5aebcd6d721150598ef32eaabb7a4738bfadcb
Pyanexy <93088367+Pyanexy@users.noreply.github.com>
2022-02-08 17:24:41 +0600

    Update ready.js

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -8,13 +8,16 @@ module.exports = async (client) => {
 
     //Game
     let statuses = ['музыке', `префикс: ${prefix}`];
+    let activ = "CONNECTED"
     setInterval(function() {
   		let status1 = statuses[Math.floor(Math.random()*statuses.length)];
-  		client.user.setActivity({ 
+  		let activity = activ[Math.floor(Math.random()*activ.length)];
+client.user.setActivity({ 
 status: "idle",
 activities: [{
 name: status1, 
-type: "COMPETING"}]
+type: activity
+}]
 });
   	}, 10000)
 
