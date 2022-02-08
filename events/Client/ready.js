2753cd9cb953420b12f01378a8db3ce66120e7cb
Pyanexy <93088367+Pyanexy@users.noreply.github.com>
2022-02-08 17:08:29 +0600

    Update ready.js

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -4,13 +4,13 @@ module.exports = async (client) => {
 
     client.manager.init(client.user.id);
     client.logger.log(`${client.user.username} Sudah online!`, "ready");
-    client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`, "ready");
+    client.logger.log(`Запущены ${client.guilds.cache.size} серверов, в общей сложности ${client.users.cache.size} пользователи`, "ready");
 
     //Game
-    let statuses = ['blacky', `Prefix : ${prefix}`];
+    let statuses = ['музыке', `префикс: ${prefix}`];
     setInterval(function() {
   		let status = statuses[Math.floor(Math.random()*statuses.length)];
-  		client.user.setActivity(status, {type: "PLAYING"});
+  		client.user.setActivity(status, {type: "COMPETING"});
   	}, 10000)
 
 }
