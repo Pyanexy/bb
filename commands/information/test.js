86166a409c32038e2057db1409c4fb62c40994e3
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-11 16:22:17 +0600

    Create test.js

---
+++ commands/information/test.js
@@ -0,0 +1,35 @@
+const { MessageActionRow, MessageSelectMenu } = require('discord.js'); 
+
+module.exports = {
+    name: "test",
+    category: "Information",
+    aliases: [ "botinfo" ],
+    description: "See description about this project",
+    args: false,
+    usage: "",
+    permission: [],
+    owner: false,
+    execute: async (message, args, client, prefix) => {
+ 
+  const row = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Nothing selected')
+					.addOptions([
+						{
+							label: 'Select me',
+							description: 'This is a description',
+							value: 'first_option',
+						},
+						{
+							label: 'You can select me too',
+							description: 'This is also a description',
+							value: 'second_option',
+						},
+					]),
+			);
+
+		message.channel.send({ content: 'Pong!', components: [row] });
+}
+}
