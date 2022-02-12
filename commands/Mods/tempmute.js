50212deaff01f92dab03fb933919b2f5bea02f23
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-12 18:53:17 +0600

    Update tempmute.js

--- commands/Mods/tempmute.js
+++ commands/Mods/tempmute.js
@@ -24,38 +24,39 @@ execute: async (message, args, client, prefix) => {
       });
     }
     if (!user) {
-      return message.channel.send("you need to specify the user");
+      return message.channel.send("Нужно указать пользователя");
     }
     if (user.id === message.owner.id) {
       return message.channel.send(
-        "You can use any Mod Command against the Server Owner"
+        "Вы можете использовать любую команду мода против владельца сервера."
       );
     }
     const time = args[0];
     if (!time) {
       return message.channel.send(
-        "How many are you going to mute that person ()"
+        "Сколько вы собираетесь заглушать этого человека ()"
       );
     }
     const reason = args.slice(1).join(" ");
     if (!reason) {
       return message.channel.send(
-        "With what reason are you going to tempmute?:"
+        "По какой причине вы собираетесь tempmute?:"
       );
     }
     const mtembde = new MessageEmbed()
-      .setTitle("Action: Tempmute")
-      .setColor("RANDOM")
-      .addField("User:", user)
-      .addField("Reason", reason)
-      .addField("Moderator:", message.member.displayName)
-      .addField("Time", time, true);
+      .setTitle("Действие: Tempmute")
+      .setColor(client.embedColor)
+      .addField("Пользователь:", user)
+      .addField("Причина", reason)
+      .addField("Модератор:", message.member.displayName)
+      .addField("Время", time, true);
     const mtuembde = new MessageEmbed()
-      .setTitle("YOU HAVE BEEN MUTED!!")
-      .setColor("RANDOM")
-      .addField("Reason", reason)
-      .addField("Moderator:", message.member.displayName)
-      .addField("Time", time, true)
+      .setTitle("ТЕБЯ ЗАГЛУШИЛИ!!")
+      .setColor(client.embedColor)
+      .addField("Причина", reason)
+      .addField("Модератор:", message.member.displayName)
+      .addField("Время", time, true)
+       user.send({ embeds: [mtuembde] });
     message.channel.send({ embeds: [mtembde] });
     user.roles.add(role);
     setTimeout(function () {
