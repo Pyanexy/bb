b0b8533ed38d5ea1bbfd61c21f8fc36b9f8fd139
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-12 20:10:16 +0600

    Create leavechannel.js

---
+++ database/guildData/leavechannel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const goodbyeSchema = new mongoose.Schema({
+  Bye: {
+    type: String,
+  },
+  GuildID: String
+});
+
+const MessageModel = module.exports = mongoose.model('goodbye', goodbyeSchema);
