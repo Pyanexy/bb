5c398d993c70ac4ae450ecd32d5b4f17c2433f48
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-13 23:22:00 +0600

    Create setChannel.js

---
+++ commands/Mods/setChannel.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "kick",
+    category: "Mods",
+    description: "Команда кик",
+    args: false,
+    usage: "",
+    permission: [],
+    owner: false,
+    execute: async (message, args, client, prefix) => {
+
+ //mod channel 
+if(!message.member.permissions.has("ADMINISTRATOR")) return message.reply(`У вас недостаточно прав для выполнения этой команды.`);
+const channel = message.mentions.channels.first();
+if(!channel) return message.reply(`Вы не указали канал для мод логов.`)
+
+client.db.set(`channel_${channel}`)
+message.channel.send(`Канал установлен: ${channel}`) 
+  }
+}
