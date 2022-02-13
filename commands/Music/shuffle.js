b429b4f71a00edd4f90c5e7dcb253e30385784ff
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-13 20:12:45 +0600

    Update shuffle.js

--- commands/Music/shuffle.js
+++ commands/Music/shuffle.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
 	name: "shuffle",
     category: "Music",
-    description: "Shuffle queue",
+    description: "Перемешать очередь",
     args: false,
     usage: "",
     permission: [],
@@ -18,7 +18,7 @@ module.exports = {
         if (!player.queue.current) {
             let thing = new MessageEmbed()
                 .setColor("RED")
-                .setDescription("There is no music playing.");
+                .setDescription("Музыка не играет.");
             return message.channel.send({embeds: [thing]});
         }
 
@@ -28,7 +28,7 @@ module.exports = {
         const emojishuffle = message.client.emoji.shuffle;
 
         let thing = new MessageEmbed()
-            .setDescription(`${emojishuffle} Shuffled the queue`)
+            .setDescription(`${emojishuffle} Перемешал очередь`)
             .setColor(message.client.embedColor)
             .setTimestamp()
         return message.channel.send({embeds: [thing]}).catch(error => message.client.logger.log(error, "error"));
