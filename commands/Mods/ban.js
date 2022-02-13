f53f59d0e7b7270c0eaaefaca155f53bcbfbd7a8
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-13 21:18:38 +0600

    Update ban.js

--- commands/Mods/ban.js
+++ commands/Mods/ban.js
@@ -1,21 +1,21 @@
 const { MessageEmbed } = require("discord.js");
 
-module.exports = {
-name: "ban",
-aliases: [],
-description: "Заблокировать пользователя на сервере",
-category: "Mods",
-used: "",
-agrs: false,
-permission: [],
-owner: false,
-execute: async (client, message, args, prefix) => {
+  module.exports = {
+   name: "ban",
+   aliases: [],
+   description: "Заблокировать пользователя на сервере",
+   category: "Mods",
+   used: "",
+   agrs: false,
+   permission: [],
+   owner: false,
+   execute: async (client, message, args, prefix) => {
 if(!message.member.permissions.has("BAN_MEMBERS")) return message.reply("У вас нет разрешения на это.");
         const user = message.mentions.users.first();
         if (!user) return message.reply(`Укажите, кого вы хотите забанить. **${prefix}ban <пользователь> [причина]**`);
         if(user.id === message.author.id) return message.reply("Вы не можете забанить себя.");
         const reason = args.slice(1).join(" ");
-        message.guild.members.cache.get(user.id).ban({reason: reason});
+        message.guild.members.cache.get(user.id).ban(reason);
  
         const banmessage = new MessageEmbed()
         .setColor(client.embedColor)
