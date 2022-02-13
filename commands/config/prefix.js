a9597bc9af3e5611f797f8d2fce55aa40e0e7843
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-13 13:00:20 +0600

    Create prefix.js

---
+++ commands/config/prefix.js
@@ -0,0 +1,46 @@
+const { MessageEmbed } = require("discord.js"); 
+
+const { default_prefix } = require(`${process.cwd()}/config.json`)
+module.exports = { 
+name: "setprefix", 
+category: "Config", 
+description: "Установить камтомный префикс", 
+args: false, 
+usage: "", 
+aliases: ["prefix"], 
+permission: [], 
+owner: false, 
+async execute(message, args, client) { 
+
+if (!message.member.permissions.has('MANAGE_GUILD')) return message.channel.send('У вас нет `Manage Guild` права для использования этой команды. '); 
+
+if (!args[0]) { 
+const embed = new MessageEmbed() 
+.setDescription("Вы не указали значения нового префикса") 
+.setColor(client.embedColor) return 
+message.channel.send({ embeds: [embed] }); } 
+
+if (args[1]) { 
+const embed = new MessageEmbed() 
+.setDescription("Вы не указали аргумент") 
+.setColor(client.embedColor) return 
+message.channel.send({ embeds: [embed] }); } 
+
+if (args[0].length > 3) { 
+const embed = new MessageEmbed() 
+.setDescription("Значение префикса не может привышать 3 символов") 
+.setColor(client.embedColor) return 
+message.channel.send({ embeds: [embed] }); } 
+
+if (args.join("") === default_prefix) { 
+client.db.delete(`prefix_${message.guild.id}`); 
+const embed = new MessageEmbed() 
+.setDescription("Сброс префикса") 
+.setColor(client.embedColor) return await message.channel.send({ embeds: [embed] }); } 
+
+client.db.set(`prefix_${message.guild.id}`, args[0]); 
+const embed = new MessageEmbed() 
+.setDescription(`Новый префикс на сервере: ${args[0]}`) 
+.setColor(client.embedColor) await message.channel.send({ embeds: [embed] });
+     },
+};
