900edb592b65498f69c875e6215f0e3e0df5b995
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-14 00:26:40 +0600

    Update unmute.js

--- commands/Mods/unmute.js
+++ commands/Mods/unmute.js
@@ -10,7 +10,10 @@ execute: async (message, args, client, prefix) => {
 //mod channel 
 if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.reply(`У вас недостаточно прав для выполнения этой команды.`);
 const users = message.mentions.members.first();
+if(!users) return message.reply("Укажите пользователя");
+
 const unmute = message.guild.roles.cache.find( r => r.name == "Заглушен")
+if(!unmute) return message.reply(`Роль не найдена(название: "Заглушен"), создайте роль используя n!create-role`)
 users.roles.remove(unmute.id)
 message.channel.send(`С ${users} сняты ограничения`) 
  }
