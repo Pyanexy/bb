cf4640bd8ffcd5d5568096d2a7ab5821531e4000
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-13 12:18:11 +0600

    Delete database directory

--- database/Schema/User.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("User", new mongoose.Schema({
-    id: { type: String },
-    registeredAt: { type: Number, default: Date.now() },
-    bio: { type: String, default: `<prefix>bio текст` },
-}));
