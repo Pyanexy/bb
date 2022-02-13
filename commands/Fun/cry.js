909fccdedef814411516fc6d33724656ac2ea5bd
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-13 11:30:06 +0600

    Update cry.js

--- commands/Fun/cry.js
+++ commands/Fun/cry.js
@@ -1,6 +1,5 @@
 const discord = require("discord.js");
-const { Random } = require("something-random-on-discord");
-const random = new Random();
+const random = require("something-random-on-discord");
 
 module.exports = { name: "cry", category: "Fun", aliases: [  ], description: "See description about this project", args: false, usage: "", permission: [], owner: false, 
 execute: async (message, args, client, prefix) => {
