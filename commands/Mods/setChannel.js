37e6e5ef2be4a24ec3a982d441c6c6622112fdb1
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-13 23:52:51 +0600

    Update setChannel.js

--- commands/Mods/setChannel.js
+++ commands/Mods/setChannel.js
@@ -10,10 +10,11 @@ module.exports = {
 
  //mod channel 
 if(!message.member.permissions.has("ADMINISTRATOR")) return message.reply(`У вас недостаточно прав для выполнения этой команды.`);
+
 const channel = message.mentions.channels.first();
 if(!channel) return message.reply(`${client.db.get(`channel_${message.guild.id}`) || `На сервере не указан канал для мод логов`}`)
-
-client.db.set(`channel_${message.guild.id}`, agrs[0] )
+let context = args.slice(0).join(` `)
+client.db.set(`channel_${message.guild.id}`, context )
 message.channel.send(`Канал установлен: ${channel}\nИндификатор канала: ${channel.id}`) 
   }
 }
