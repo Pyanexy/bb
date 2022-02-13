a9776e71272a5b57f88865747c2712eba02c3606
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-13 13:09:14 +0600

    Create bio.js

---
+++ commands/Utility/bio.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "bio",
+    category: "Utility",
+    description: "Установить биографию",
+    args: false,
+    usage: "",
+    aliases: [],
+    permission: [],
+    owner: false,
+    async execute(message, args, client, prefix) {
+      
+       if (!args[0]) {
+    const embed = new MessageEmbed()
+        .setDescription(`${client.db.bio || `Вы можете установить что-то сюда используя ${prefix}bio ваш тек`}`)
+        .setColor(client.embedColor)
+      return message.channel.send({ embeds: [embed] });
+    }
+
+
+    if (args[0].length > 200) {
+       const embed = new MessageEmbed()
+        .setDescription("Биография может достигать до 200 символов, не больше")
+        .setColor(client.embedColor)
+      return message.channel.send({ embeds: [embed] });
+    }
+    
+    client.db.set(`bio_${message.author.id}, agrs[0]) 
+   const embed = new MessageEmbed()
+   .setDescription(`Ура, вы установили свою биографию, посмотреть можно здесь \`${prefix}userinfo\``)
+   .setColor(client.embedColor)
+   message.react("🎉");
+   message.channel.send({ embeds: [embed] })
+   }
+}
