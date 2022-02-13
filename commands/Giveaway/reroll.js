64a34c36de520eafc34e405274db8048ebb7b2af
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-13 14:13:23 +0600

    Update reroll.js

--- commands/Giveaway/reroll.js
+++ commands/Giveaway/reroll.js
@@ -1,18 +1,25 @@
 const { MessageEmbed } = require('discord.js')
 
-module.exports = {
-    name : 'reroll',
-    run : async(client, message, args) => {
-        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You do not have permission')
+module.exports = { 
+name: "reroll", 
+category: "Giveaway", 
+description: "Определить другово победителя", 
+args: false, 
+usage: "", 
+aliases: [], 
+permission: [], 
+owner: false, 
+async execute(message, args, client) {
+        if(!message.member.permissions.has('MANAGE_MESSAGES')) return message.channel.send('У вас недостаточно прав!')
 
-        if(!args[0]) return message.channel.send('Please specify a message id')
+        if(!args[0]) return message.channel.send('Пожалуйста, укажите идентификатор сообщения')
 
         const giveaway = client.giveaways.giveaways.find((g) => g.messageID === args[0]);
-        if(!giveaway) return message.channel.send('Couldn\'t find the giveaway.')
+        if(!giveaway) return message.channel.send('Не удалось найти подарок.')
 
         client.giveaways.reroll(giveaway.messageID)
             .then(() => {
-                message.channel.send("Giveaway rerolled");
+                message.channel.send("Розыгрыш повторен");
             })
             .catch(err => {
                 console.log(err)
