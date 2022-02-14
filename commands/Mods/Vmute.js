50b5a3a91b4ff633c46e2b2a6a365a33c531430c
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-14 16:14:33 +0600

    Update Vmute.js

--- commands/Mods/Vmute.js
+++ commands/Mods/Vmute.js
@@ -12,12 +12,12 @@ owner: false,
 execute: async (message, args, client, prefix) => {
 //mod channel 
  if(!args[0]) return message.reply("Выберите тип: \`text || voice\`");
-    if(args.slice(1).join(" ") == "text") {
+    if(args.slice(0) == "text") {
      if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send("You have insufficient permissions to execute this command.");
   let muteUser = message.mentions.members.first();
   if(!muteUser) return message.channel.send("Couldn't find user | **Usage:** `>mute @user <time> <reason>`");
   if(muteUser.permissions.has("ADMINISTRATOR")) return message.channel.send(":clown: You tried. :clown:");
-  let reason = args.slice(3).join(" ");
+  let reason = args.slice(2).join(" ");
   if(!reason) return message.channel.send("Specify a reason | **Usage:** `>mute @user <time> <reason>`");
 
   let muterole = message.guild.roles.cache.find(r => r.name === "Заглушен")
@@ -40,7 +40,7 @@ execute: async (message, args, client, prefix) => {
       console.log(e.stack);
     }
   }
-  let length = args[2];
+  let length = args[1];
   if(isNaN(ms(length))) return message.channel.send("Укажите время. **Использование:** `n!mute @user <время> <причина>`");
   message.delete().catch();
 
@@ -51,7 +51,7 @@ setTimeout(function(){
     muteUser.roles.remove(muterole.id);
    
 }, ms(length));
-} else if(args.slice(1).join(" ") == "voice") {
+} else if(args.slice(0) == "voice") {
 
  const member = message.mentions.members.first();
 
@@ -60,7 +60,7 @@ setTimeout(function(){
  if(message.member.roles.highest.position < member.roles.highest.position ) return message.reply("Ваша роль ниже егошний роли");
  if(member.voice.selfMute || member.voice.mute) return message.reply("Пользователь уже заглушен");
  if(!member.voice.channel) return message.reply("Пользователь не находится в войс канале");
- const reason = args.slice(3).join(" ")
+ const reason = args.slice(1).join(" ")
  if(!reason) return message.reply("Укажите причину");
 
  await member.voice.setMute(true, reason).then(() => {
