cef683b82a317597a5c050b98350e5a4d95c48f8
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-15 14:06:15 +0600

    Update bio.js

--- commands/Utility/bio.js
+++ commands/Utility/bio.js
@@ -14,7 +14,7 @@ execute: async (client, message, args, prefix) => {
 if(!args[0]) return message.reply("Твоя биография:" + await client.db.get(`bio_${message.author.id}`) || "нету")
 
 let context = args.slice(0).join(" ")
-if(!context.length =< 200) return message.reply("Биография не может быть длиннее 200 букв")
+if(!context.length >= 200) return message.reply("Биография не может быть длиннее 200 букв")
 client.db.set(`bio_${message.author.id}`, context)
 const embed = new MessageEmbed()
 .setColor(client.embedColor)
