00f24543494d15b44836505c743744baca05d376
Pyanexx <93088367+Pyanexy@users.noreply.github.com>
2022-02-15 14:29:02 +0600

    Update bio.js

--- commands/Utility/bio.js
+++ commands/Utility/bio.js
@@ -13,7 +13,7 @@ execute: async (client, message, args, prefix) => {
 let context = args.slice(0).join(` `)
     
     if(context.length > 0) return message.reply("Биография не может быть пустым значением")
-    if(context.length >= 200) return message.reply(`К сожелению я не могу поставить вам такое описание. Оно имеет
+    if(context.length >= 200) return message.reply(`К сожелению я не могу поставить вам такое описание. Оно имеет более 200 букв")
 client.db.set(`bio_${message.author.id}`, context)
 const embed = new MessageEmbed()
 .setColor(client.embedColor)
